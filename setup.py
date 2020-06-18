89c6725a943a90f93a9b3c01539e0cc1e31426d7
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-06-18 16:53:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='3.4 or Higher',
+    python_requires='>=3.4',
     test_suite='nose.collector',
     tests_require=['nose']
 )
