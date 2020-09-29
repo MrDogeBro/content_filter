6321018c1619aff79d67d3482c2cb0f285880c31
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-09-29 14:21:08 -0400

    Added check w/ list output and moved checks
    Added a check that outputs a list of the words it found, their counts, and their censored versions. Moved checks to checks.py to take up less space in the main file.

--- content_filter/filter.py
+++ content_filter/filter.py
@@ -1,13 +1,16 @@
 # filter.py
 
-import re
 import json
 import os
+
+from .check import defaultCheck, listCheck
 
 exceptionList = []
 additionalList = []
 customWordList = []
 useDefaultList = True
+useCustomFile = False
+
 
 def _makeListsLower(listName):
     if listName == 'exceptionList':
@@ -21,34 +24,39 @@ def _makeListsLower(listName):
     else:
         pass
 
+
 def _changeListChars(listName):
     if listName == 'exceptionList':
         global exceptionList
-        exceptionList = [i.replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace('!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#') for i in exceptionList]
+        exceptionList = [i.replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace(
+            '!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#') for i in exceptionList]
 
     elif listName == 'additionalList':
         global additionalList
-        additionalList = [i.replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace('!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#') for i in additionalList]
+        additionalList = [i.replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace(
+            '!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#') for i in additionalList]
 
     elif listName == 'customList':
         global customWordList
-        customWordList = [i.replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace('!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#').replace(' ', '') for i in customWordList]
+        customWordList = [i.replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace('!', 'i').replace(
+            '@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#').replace(' ', '') for i in customWordList]
 
     else:
         pass
+
 
 def useCustomList(wordList):
     """Allows the user to define a custom list of words to filter for.
     Keep in mind that this will completely override the default filter,
     meaning that any words defined in the default filter will now have
     to be manually added by the user if they desire for those words to
-    be filtered. If you would just like to add on or remove from the 
+    be filtered. If you would just like to add on or remove from the
     existing, please check out our [GitHub](https://github.com/MrDogeBro/content_filter)
 
     Paramaters:
     ----------
     words:
-        The words that the filter will check for. This can be in the 
+        The words that the filter will check for. This can be in the
         form of a single string or an array of strings.
     """
     global customWordList
@@ -56,12 +64,13 @@ def useCustomList(wordList):
 
     if isinstance(wordList, list):
         customWordList = wordList
-    
+
     else:
         customWordList = [wordList]
 
     useDefaultList = False
     _changeListChars('customList')
+
 
 def addExceptions(words=None):
     """Allows the user to remove words to the list of pre-defined words
@@ -71,7 +80,7 @@ def addExceptions(words=None):
     Paramaters:
     ----------
     words:
-        The words that will be removed from the default filter to 
+        The words that will be removed from the default filter to
         check for. This can be in the form of a single string or an
         array of strings.
     """
@@ -85,6 +94,7 @@ def addExceptions(words=None):
 
     _makeListsLower('exceptionList')
 
+
 def addWords(words=None):
     """Allows the user to add words to the list of pre-defined words
     to filter for. If you would like to completely override the existing
@@ -93,20 +103,48 @@ def addWords(words=None):
     Paramaters:
     ----------
     words:
-        The words that will be added to the default filter to check 
-        for. This can be in the form of a single string or an array 
+        The words that will be added to the default filter to check
+        for. This can be in the form of a single string or an array
         of strings.
     """
     global additionalList
 
     if isinstance(words, list):
         additionalList.extend(words)
-    
+
     else:
         additionalList.extend([words])
 
     _makeListsLower('additionalList')
     _changeListChars('additionalList')
+
+
+def useCustomListFile(file, currentFile):
+    """Allows the user to define a custom list of words to filter for
+    from a json file. Keep in mind that this will completely override
+    the default filter, meaning that any words defined in the default
+    filter will now have to be manually added by the user if they
+    desire for those words to be filtered. For the structure the json
+    file needs to follow or if you would just like to add on or remove
+    from the existing list, please check out our [GitHub](https://github.com/MrDogeBro/content_filter)
+
+    Paramaters:
+    ----------
+    file:
+        A json file that defines a list of words to filter for. To find
+        out the json file structure, check out our Github (linked above)
+    """
+
+    global useCustomFile, useDefaultList
+
+    customJSONFile = os.path.join(os.path.dirname(
+        os.path.abspath(currentFile)), file)
+
+    with open(customJSONFile) as f:
+        useCustomFile = json.load(f)
+
+    useDefaultList = False
+
 
 def checkMessage(message):
     """Checks the provided message for any words that should be filtered and
@@ -121,86 +159,39 @@ def checkMessage(message):
 
     Returns:
     -------
-        Returns a `bool` value. True is returned if the filter found 
+        Returns a `bool` value. True is returned if the filter found
         something in the text provided. False is returned if the filter
         did not find anything of interest in the text provided.
     """
-    if useDefaultList:
-        filterContentFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/filter.json')
 
-        filterMsgContent = message.lower()
+    filterContentFile = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'data/filter.json')
 
-        # gets all of the words to filter for
-        with open(filterContentFile) as f:
-            filterData = json.load(f)
+    return defaultCheck(message, customWordList, exceptionList, additionalList,
+                        useDefaultList, useCustomFile, filterContentFile)
 
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in filterData['dontFilter']:
-            # checks for words that should not be filtered in teh message
-            if word in message.lower():
-                filterMsgContent = filterMsgContent.replace(word, '') # gets rid of the words that shouldn't be filtered so that the filter wont find them
 
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in exceptionList:
-            # checks for words that should not be filtered in teh message
-            for string in filterData['mainFilter']:
-                if string in exceptionList:
-                    try:
-                        filterData['mainFilter'].pop(filterData['mainFilter'].index(string))
+def checkMessageList(message):
+    """Checks the provided message for any words that should be filtered and
+    returns a list of words that were identified with some data about the word.
 
-                    except:
-                        filterMsgContent = filterMsgContent.replace(word, '') # gets rid of the words that shouldn't be filtered so that the filter wont find them
+    Paramaters:
+    ----------
+    message:
+        The message that should be scanned for any language that matches
+        any of the language that the filter is looking for. This value
+        should be passed in as a string.
 
-            # checks for words that should not be filtered in teh message
-            try:
-                filterData['conditionalFilter'].pop(word)
+    Returns:
+    -------
+        Returns a `list` value. List with words found, count of the words
+        found, and a censored version of the words is returned if the filter
+        found something in the text provided. False is returned if the filter
+        did not find anything of interest in the text provided.
+    """
 
-            except:
-                filterMsgContent = filterMsgContent.replace(word, '') # gets rid of the words that shouldn't be filtered so that the filter wont find them
+    filterContentFile = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'data/filter.json')
 
-        # sets up a var that will be used to look for words in that replaces all irregular charaters with the charater they might be used for as a bad word
-        filterMsgContent = filterMsgContent.replace('a$', 'a##').replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace('!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#')
-
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in filterData['mainFilter']:
-            if (re.search('+[.!-]*'.join(c for c in word), filterMsgContent.replace(' ', ''))):
-                return True # exits the check so that it doesn't fire multiple 
-                
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in additionalList:
-            if (re.search('+[.!-]*'.join(c for c in word), filterMsgContent.replace(' ', ''))):
-                return True # exits the check so that it doesn't fire multiple times
-
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in filterData['conditionFilter']:
-            if ' ' + word in filterMsgContent or word in filterMsgContent[:len(word)]:
-                return True # exits the check so that it doesn't fire multiple times
-
-        return False
-
-    else:
-        filterMsgContent = message.lower()
-
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in exceptionList:
-            # checks for words that should not be filtered in teh message
-            try:
-                customWordList.pop(word)
-
-            except:
-                filterMsgContent = filterMsgContent.replace(word, '') # gets rid of the words that shouldn't be filtered so that the filter wont find them
-
-        # sets up a var that will be used to look for words in that replaces all irregular charaters with the charater they might be used for as a bad word
-        filterMsgContent = filterMsgContent.replace('a$', 'a##').replace('"', '').replace(',', '').replace('.', '').replace('-', '').replace("'", '').replace('+', 't').replace('!', 'i').replace('@', 'a').replace('1', 'i').replace('0', 'o').replace('3', 'e').replace('$', 's').replace('*', '#')
-
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in customWordList:
-            if (re.search('+[.!-]*'.join(c for c in word), filterMsgContent.replace(' ', ''))):
-                return True # exits the check so that it doesn't fire multiple 
-                
-        # goes through all of the words in the filter and checks if any are in the message
-        for word in additionalList:
-            if (re.search('+[.!-]*'.join(c for c in word), filterMsgContent.replace(' ', ''))):
-                return True # exits the check so that it doesn't fire multiple times
-
-        return False
+    return listCheck(message, customWordList, exceptionList, additionalList,
+                     useDefaultList, useCustomFile, filterContentFile)
