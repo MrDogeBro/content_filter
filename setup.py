303303dc2df4ce90931c44c8e2a25b4e3d77a0c4
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-10-16 15:14:06 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('pypi/README.md', 'r') as fh:
 
 setuptools.setup(
     name='content-filter',
-    version='1.1.1',
+    version='1.1.2',
     author='MrDogeBro',
     author_email='bornoffire54@gmail.com',
     description='A basic but robust content filter for python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/MrDogeBro/content_filter',
-    download_url='https://github.com/MrDogeBro/content_filter/archive/v1.1.1.tar.gz',
+    download_url='https://github.com/MrDogeBro/content_filter/archive/v1.1.2.tar.gz',
     packages=setuptools.find_packages(),
     package_data={'content_filter': ['data/filter.json']},
     license='MIT',
