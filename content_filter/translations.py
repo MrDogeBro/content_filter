d77088f539f7dbe1c9debc8217d6eaae67c866a7
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-10-25 09:00:01 -0400

    Encode and decode the normalization
    Encode the output of the normalization to get the normal charater out of charaters like ç to make them just c so they can be recognised by the filter. Then we decode it to turn it from bytes to a string.

--- content_filter/translations.py
+++ content_filter/translations.py
@@ -10,7 +10,8 @@ from unicodedata import normalize
 
 def return_translated(translation_table: dict, string: str):
     translated_str = string.translate(translation_table['single'])
-    translated_str = normalize('NFKD', translated_str).lower()
+    translated_str = normalize('NFKD', translated_str).encode(
+        'ascii', 'ignore').decode().lower()
     translated_str = re.compile('|'.join(map(re.escape, translation_table['multi']))).sub(
         lambda match: translation_table['multi'][match.group(0)], translated_str)
 
