1f7d8e5dcc86af72209de1f1feb7f92eff408448
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-11-04 19:59:28 -0500

    Add logic for finding possibilities and returning them

--- content_filter/string.py
+++ content_filter/string.py
@@ -5,6 +5,7 @@ Handles the translations of charaters in messages
 """
 
 import re
+from itertools import combinations
 from unicodedata import normalize
 
 
@@ -18,5 +19,39 @@ def return_translated(translation_table: dict, string: str):
     return translated_str
 
 
-def return_possibilities():
-    pass
+def return_possibilities(message):
+    chars = []
+    chars_combined = []
+    combos = []
+    results = []
+
+    found_statements = re.findall(r'(?<=\().+?(?=\))', message)
+
+    for statement in found_statements:
+        chars.append((statement[:1], statement[2:]))
+
+    for index, char in enumerate(chars):
+        for letter in char:
+            letter += f'{index}'
+            chars_combined.append(letter)
+
+    for combo in combinations(chars_combined, len(chars)):
+        found_dup = False
+
+        for letter in combo:
+            if [letter[1:2] for letter in combo].count(letter[1:2]) > 1:
+                found_dup = True
+
+        if not found_dup:
+            combos.append(tuple(letter[:1] for letter in combo))
+
+    for combo in combos:
+        modified_message = message
+
+        for index, letter in enumerate(combo):
+            modified_message = modified_message.replace(
+                f'({found_statements[index]})', letter)
+
+        results.append(modified_message)
+
+    return results
