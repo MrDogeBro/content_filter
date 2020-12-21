ce45db63d3895790d18eed90a891911daec0d5d0
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-12-21 12:20:11 -0500

    Add custom css and js files to support custom styles and dark theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,6 +73,11 @@ html_theme_options = {
     "titles_only": False,
 }
 
+html_static_path = ["_static"]
+
+html_css_files = ["css/custom.css", 'css/dark.css']
+html_js_files = ["js/custom.js"]
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
