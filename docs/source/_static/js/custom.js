8ce7ddee133bd8c4eca342ba1d569913e4dc482f
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-12-21 12:19:41 -0500

    Create logic to have dark and light theme preferances

---
+++ docs/source/_static/js/custom.js
@@ -0,0 +1,52 @@
+const loadTheme = () => {
+  let theme = localStorage.getItem('theme');
+
+  if (theme !== null) {
+    if (theme === 'dark')
+      document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+};
+
+loadTheme();
+
+const createThemeSwitcher = () => {
+  let btn = document.createElement('BUTTON');
+  btn.className = 'theme-switcher';
+  btn.id = 'themeSwitcher';
+  btn.innerHTML =
+    '<i id=themeMoon class="fas fa-moon"></i><i id=themeSun class="fas fa-sun"></i>';
+  document.body.appendChild(btn);
+
+  if (localStorage.getItem('theme') === 'dark') $('#themeMoon').hide(0);
+  else $('#themeSun').hide(0);
+};
+
+$(document).ready(() => {
+  $('head').append(
+    '<script src="https://kit.fontawesome.com/ea3a6b8494.js" crossorigin="anonymous"></script>'
+  );
+  createThemeSwitcher();
+
+  $('#themeSwitcher').click(switchTheme);
+});
+
+const switchTheme = () => {
+  if (localStorage.getItem('theme') === 'dark') {
+    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
+
+    $('#themeSun').fadeOut(200, () => {
+      $('#themeMoon').fadeIn(200);
+    });
+  } else {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+
+    $('#themeMoon').fadeOut(200, () => {
+      $('#themeSun').fadeIn(200);
+    });
+  }
+};
