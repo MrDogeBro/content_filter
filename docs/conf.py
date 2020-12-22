624231c47f106dc9bf7dccc35033b40b96534202
MrDogeBro <MrDogeBro@users.noreply.github.com>
2020-12-21 20:18:12 -0500

    Fix directory structure for docs to build

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = [] # type: list
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -77,8 +77,3 @@ html_static_path = ["_static"]
 
 html_css_files = ["css/custom.css", 'css/dark.css']
 html_js_files = ["js/custom.js"]
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
