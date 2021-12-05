b62c1bea34950406ad58477f068951566aaf345d
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-12-05 09:27:06 +0000

    Update workflow + requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 import content_filter
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -11,25 +11,25 @@ setuptools.setup(
     author=content_filter.__author__,
     description=content_filter.__description__,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/MrDogeBro/content_filter',
-    download_url=f'https://github.com/MrDogeBro/content_filter/archive/v{content_filter.__version__}.tar.gz',
+    long_description_content_type="text/markdown",
+    url="https://github.com/MrDogeBro/content_filter",
+    download_url=f"https://github.com/MrDogeBro/content_filter/archive/v{content_filter.__version__}.tar.gz",
     packages=setuptools.find_packages(),
-    package_data={'content_filter': ['data/filter.json', 'data/replacements.json']},
+    package_data={"content_filter": ["data/filter.json", "data/replacements.json"]},
     license=content_filter.__license__,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Development Status :: 5 - Production/Stable",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4',
-    test_suite='nose.collector',
-    tests_require=['nose']
+    python_requires=">=3.5",
+    test_suite="nose.collector",
+    tests_require=["nose"],
 )
